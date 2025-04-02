d47d3e4d4a3024b2fff2d4c8c0f5dfc10deaccf5
MeridjanShoti <meridjanshoti@gmail.com>
2025-04-02 16:48:06 +0200

    nuove funzioni

---
+++ src/main/java/it/epicode/entities/mezzi/InterventoManutenzione.java
@@ -0,0 +1,51 @@
+package it.epicode.entities.mezzi;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+@Entity
+public class InterventoManutenzione {
+    @Id
+    private LocalDate inizioManutenzione;
+    private LocalDate fineManutenzione;
+    @ManyToOne
+    private Mezzo mezzoInManutenzione;
+
+    public InterventoManutenzione(LocalDate inizioManutenzione, LocalDate fineManutenzione, Mezzo mezzoInManutenzione) {
+        this.inizioManutenzione = inizioManutenzione;
+        this.fineManutenzione = fineManutenzione;
+        this.mezzoInManutenzione = mezzoInManutenzione;
+    }
+    public InterventoManutenzione() {
+    }
+
+    public LocalDate getInizioManutenzione() {
+        return inizioManutenzione;
+    }
+
+    public void setInizioManutenzione(LocalDate inizioManutenzione) {
+        this.inizioManutenzione = inizioManutenzione;
+    }
+
+    public LocalDate getFineManutenzione() {
+        return fineManutenzione;
+    }
+
+    public void setFineManutenzione(LocalDate fineManutenzione) {
+        this.fineManutenzione = fineManutenzione;
+    }
+
+    public Mezzo getMezzoInManutenzione() {
+        return mezzoInManutenzione;
+    }
+
+    public void setMezzoInManutenzione(Mezzo mezzoInManutenzione) {
+        this.mezzoInManutenzione = mezzoInManutenzione;
+    }
+    @Override
+    public String toString() {
+        return "inizioManutenzione=" + inizioManutenzione +
+                ", fineManutenzione=" + fineManutenzione +
+                ", mezzoInManutenzione=" + mezzoInManutenzione ;
+    }
+}
